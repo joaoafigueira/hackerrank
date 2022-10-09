21e437d0a63180cdcc31f333a3f106e4803fac05
IATec - João Victor Alves Figueira <joao.figueira@iatec.com>
2022-10-09 16:54:10 -0300

    hackerrank java substring solution

--- src/javaproblems/JavaSubstring.java
+++ src/javaproblems/JavaSubstring.java
@@ -1,2 +1,22 @@
-package javaproblems;public class JavaSubstring {
+package javaproblems;
+
+import java.util.Scanner;
+
+/* Hacker Rank Substring problem */
+
+public class JavaSubstring {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        String s = scan.next();
+        int start = scan.nextInt();
+        int end = scan.nextInt();
+
+        System.out.println(s.substring(start,end));
+
+    }
+
+
 }
